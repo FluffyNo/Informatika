612ddc78bc53f77d2810505a37925c6d0868e3b1
alekseevN <nikita.alekseev.2019@gmail.com>
2024-01-18 14:34:00 +0800

    123

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-arg1 = float(input('Введите сторону квадрата: '))
-p = arg1*4
-print("Периметр квадрата =", p)
+# arg1 = float(input("Введите что-то там: "))
+# arg2 = float(input("Введите что-то там 2: "))
+# print("Площадь",arg1 * 2 + arg2 * 2)
+# print("Периметр", arg1+arg2 * 2 )
+# print("Площадь", f"{arg1} * 2 + {arg2} * 2 = {arg1 * 2 + arg2 * 2}")
+# print("Периметр", f"{arg1} + {arg2} * 2 = {arg1 + arg2 * 2}")
+
+import math
+#
+# arg1 = int(input('Введите число: '))
+# calc = math.sqrt(arg1)
+# print('Корень:',calc)
+arg1 = float(input('Введите что-то: '))
+print('Ответ', math.pi * arg1)
