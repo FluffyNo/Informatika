ba3f01c5f2878c64c8e98b24c012e08d3ca82f99
alekseevN <nikita.alekseev.2019@gmail.com>
2024-01-24 13:18:16 +0800

    задание #2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-# arg1 = float(input("Введите что-то там: "))
-# arg2 = float(input("Введите что-то там 2: "))
-# print("Площадь",arg1 * 2 + arg2 * 2)
-# print("Периметр", arg1+arg2 * 2 )
-# print("Площадь", f"{arg1} * 2 + {arg2} * 2 = {arg1 * 2 + arg2 * 2}")
-# print("Периметр", f"{arg1} + {arg2} * 2 = {arg1 + arg2 * 2}")
-
-import math
+# # arg1 = float(input("Введите что-то там: "))
+# # arg2 = float(input("Введите что-то там 2: "))
+# # print("Площадь",arg1 * 2 + arg2 * 2)
+# # print("Периметр", arg1+arg2 * 2 )
+# # print("Площадь", f"{arg1} * 2 + {arg2} * 2 = {arg1 * 2 + arg2 * 2}")
+# # print("Периметр", f"{arg1} + {arg2} * 2 = {arg1 + arg2 * 2}")
 #
-# arg1 = int(input('Введите число: '))
-# calc = math.sqrt(arg1)
-# print('Корень:',calc)
-arg1 = float(input('Введите что-то: '))
-print('Ответ', math.pi * arg1)
+# import math
+# #
+# # arg1 = int(input('Введите число: '))
+# # calc = math.sqrt(arg1)
+# # print('Корень:',calc)
+# arg1 = float(input('Введите что-то: '))
+# print('Ответ', math.pi * arg1)
+import math
+
+a = float(input('Введите A '))
+b = float(input('Введите B '))
+x = float(input('Введите X '))
+print('y =', math.pow(math.sin( (math.pow(x, 2) + a)**2), 3) - math.sqrt(x/b))
+print('z =', math.pow(x, 2)/a + math.cos((x+b)**3))
